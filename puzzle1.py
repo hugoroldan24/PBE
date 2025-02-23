fdd5d5088cd9f3a1415f59ba6745419e9834dab1
hugoroldan24 <hugoroldan060@gmail.com>
2025-02-23 13:40:51 +0100

    Update puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -1,1 +1,5 @@
-
+"""
+El primer puzzle consisteix en imprimir per consola el uid (user identifier) de la tarjeta o clauer Mifare S50 o la tarjeta de la UPC.
+Utilitzaré la llibreria SimpleMFRC522 del paquet de llibreries mfrc522. 
+Per instalarla, he 
+""" 
