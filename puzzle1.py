08b26deb034854ad522bf151d1f071457b76ac94
hugoroldan24 <hugoroldan060@gmail.com>
2025-02-26 12:27:24 +0100

    Update puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -5,7 +5,7 @@ Per instalarla, he fet servir la següent comanda a la terminal:
 pip install mfrc522-python 
 Descarrego la llibreria desde el repositori en linea de python PyPI. Aquesta llibreria et fa interactuar amb el mòdul des de un nivell molt alt, però per l'aplicació demanada no és necessari programar-ho a un nivell més baix.
 """ 
-from mfrc522 import SimpleMFRC522                    #Importo la llibreria SimpleMFRC522 del paquet mfrc522
+from mfrc522 import SimpleMFRC522                     #Importo la llibreria SimpleMFRC522 del paquet mfrc522
 class Rfid_522:
   """Mètode constructor que s'executa quan instanciem un objecte de la classe Rfid_522"""
   def __init__(self):
