c3e58c48bef4b2f35a490723c0b7bfd73b0264d7
hugoroldan24 <hugoroldan060@gmail.com>
2025-02-26 11:47:39 +0100

    Update puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -1,5 +1,22 @@
 """
 El primer puzzle consisteix en imprimir per consola el uid (user identifier) de la tarjeta o clauer Mifare S50 o la tarjeta de la UPC.
 Utilitzaré la llibreria SimpleMFRC522 del paquet de llibreries mfrc522. 
-Per instalarla, he 
+Per instalarla, he fet servir la comanda:
+pip install mfrc522-python 
+Descarrego la llibreria desde el repositori en linea PyPI. Aquesta llibreria et fa interactuar amb el mòdul des de un nivell molt alt, però per l'aplicació demanada no és necessari programar-ho a un nivell més baix.
+
 """ 
+from mfrc522 import SimpleMFRC522          #Importo la llibreria SimpleMFRC522 del paquet mfrc522
+
+class Rfid_522:
+  def __init__(self):
+      self.uid = None                            # Declarem l'atribut uid i l'inicializtem com a None 
+      self.reader = SimpleMFRC522()              # Instanciem un objecte de la clase SimpleMFRC522()
+  """ Llegueix el UID i ho guarda en format hexadecimal """
+  def read_uid(self): 
+      self.uid = hex(self.reader.read_id())[2:]  # Utilitzem el mètode read_id() de la llibreria SimpleMFRC522 que retorna l'id en format Integer. Després el convertim a hexadecimal i eliminem el prefixe "0x". 
+if __name__ == "__main__":                       # Aquesta linea permet que el codi no s'executi automàticament si l'importem com a mòdul en un altre script. El codi només s'executarà quan el fem correr directament escribint a la terminal python puzzle1.py.
+      rf = Rfid_522()
+      rf.read_uid()                              # Obtenim el valor de l'uid en hexadecimal, que es guardarà a l'atribut uid.
+      print(f"uid: {rf.uid}")                    # Imprimim l'identificador en format hexadecimal per pantalla
+    
