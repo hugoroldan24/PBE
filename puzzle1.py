301aa1b513239993b7fdad672252d8260793bcc5
hugoroldan24 <hugoroldan060@gmail.com>
2025-03-01 18:55:15 +0100

    Update puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -14,7 +14,7 @@ class Rfid_522:
       self.reader = SimpleMFRC522()                    # Instanciem un objecte de la clase SimpleMFRC522()
   """Mètode bloquejant (mètode read_id de la llibreria SimpleMFRC522 és bloquejant) que llegueix el uid i ho guarda en format hexadecimal a l'atribut "uid" de la pròpia classe"""
   def read_uid(self): 
-      self.uid = hex(self.reader.read_id())[2:].upper()# Utilitza el mètode read_id() de la llibreria SimpleMFRC522 sobre l'objecte reader que retorna l'id en format Integer. Després el convertim a hexadecimal, eliminem el prefixe "0x" i ho posem en majúscules amb el mètode upper(). 
+      self.uid = hex(self.reader.read_id())[2:][:8].upper()# Utilitza el mètode read_id() de la llibreria SimpleMFRC522 sobre l'objecte reader que retorna l'id en format Integer. Després el convertim a hexadecimal, eliminem el prefixe "0x", ens quedem amb el 8 primers dígits i ho posem en majúscules amb el mètode upper(). 
 if __name__ == "__main__":                             # Aquesta linea permet que el codi no s'executi automàticament si l'importem com a mòdul en un altre script (quan fem el puzzle2 per exemple). El codi només s'executarà quan l'executem directament escribint a la terminal: python puzzle1.py.
       rf = Rfid_522()                                  # Instancia un objecte de la classe Rfid_522()
       rf.read_uid()                                    # Obté el valor de l'uid en hexadecimal, que es guardarà a l'atribut uid. Com el mètode és bloquejant, no s'impimirà res fins que no apropem el clauer i obtenim una uid.
