61352dd59dac9756ff8e0ab8cea485e5bb5efbf6
hugoroldan24 <hugoroldan060@gmail.com>
2025-03-08 00:11:40 +0100

    Update puzzle2.py

--- puzzle2.py
+++ puzzle2.py
@@ -9,8 +9,63 @@ import puzzle1 as pz1
 gi.require_version("Gtk", "3.0")  #Indiquem que volem fer servir GTK3
 from gi.repository import Gtk     #Importem desde el repositori de gi la llibreria Gtk que conté les classes y mètodes per crear la interfaç. 
 
-class Finestra(Gtk.Window)        #Clase relacionada amb la finestra de la aplicació. La classe hereda la clase Gtk.Window de gtk3
-    def __init__:
+class Finestra(Gtk.Window):        #Clase relacionada amb la finestra de la aplicació. La classe hereda la clase Gtk.Window de gtk3
+    def __init__(self,widgetManager):
+        super().__init__()        #Truquem a la funció __init__ de la classe Gtk.Window
+        self.set_title("PUZZLE2") 
+        self.set_default_size(400,300)
+        self.set_position(Gtk.WindowPosition.CENTER) #Accedeixes a una constant pròpia de Gtk 
+        self.widgetManager = widgetManager
+
+class widgetManager:
+    def __init__(self):
+        self.boxes = []
+        
+    def crear_label(self,text):
+        return Gtk.Label(label=text)
+        
+    def crear_box(self,direccio):
+        box = Gtk.Box(orientation=direccio,spacing=1)
+        self.boxes.append(box)
+        return box
+        
+    def crear_boton(self,text):
+        return Gtk.Button(label=text)
+        
+    def afegir_widget_box(self,box,widget,expand,fill,padding):
+        box.pack_start(widget, expand, fill, padding)
+        
+class widgetEditor:
+    def __init__(self)
+        editor_css = Gtk.CssProvider()         #Creem l'objecte que controlarà les regles d'estil CSS
+    def configurar_estil(widget,color_fons,color_text,padding,border_radius)
+        css = f"""
+        *{{
+            background-color: {color_fons};  
+            color: {color_text};                  
+            padding: {padding}px;                  
+            border-radius: {border_radius}px;   
+        }}
+        """
+        self.editor_css.load_from_data(css.encode())
+        
+        
+            
+        self.css_provider.load_from_data
+
+
+class Background:
+    def __init__(self)
+        self.eventboxes = []
+    def crear_label(text)
+        return intro_label= Gtk.Label(label=text)
+    def crear_eventbox(self)
+        return Gtk.EventBox()
+    def afegir_label(label,eventbox)
+        eventbox.add(label)
+    
+
+                    
 if __name__ == "__main__":
   
 
