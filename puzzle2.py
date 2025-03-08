6b51fd449fc717469fefcb8d47f12f09f9707fc3
hugoroldan24 <hugoroldan060@gmail.com>
2025-03-08 21:43:25 +0100

    Update puzzle2.py

--- puzzle2.py
+++ puzzle2.py
@@ -6,52 +6,52 @@ sudo apt install gir1.2-gtk-3.0
 """
 import gi
 import puzzle1 as pz1
-gi.require_version("Gtk", "3.0")  #Indiquem que volem fer servir GTK3
-from gi.repository import Gtk     #Importem desde el repositori de gi la llibreria Gtk que conté les classes y mètodes per crear la interfaç. 
+gi.require_version("Gtk", "3.0")                          #Indiquem que volem fer servir GTK3
+from gi.repository import Gtk                             #Importem desde el repositori de gi la llibreria Gtk que conté les classes y mètodes per crear la interfaç. 
 
-class Finestra(Gtk.Window):        #Clase relacionada amb la finestra de la aplicació. La classe hereda la clase Gtk.Window de gtk3
+class MyWindow(Gtk.Window):                                #Clase relacionada amb la finestra de la aplicació. La classe hereda la clase Gtk.Window de gtk3
     def __init__(self,widgetManager,widgetEditor):
-        super().__init__()                        #Truquem a la funció __init__ de la classe Gtk.Window     
-        self.wM = widgetManager                    #Instanciem un objecte de la clase widgetManager, s'encarregarà de gestionar tot lo relacionat amb els wadgets
+        super().__init__()                                 #Truquem a la funció __init__ de la classe Gtk.Window     
+        self.wM = widgetManager()                            #Instanciem un objecte de la clase widgetManager, s'encarregarà de gestionar tot lo relacionat amb els wadgets
         
     def configure_window(self,amplada,altura,posició,titol):
         self.set_title(titol) 
         self.set_default_size(amplada,altura)
         self.set_position(posició) 
-               
-    def exit_button_pressed(self):
-        Gtk.main_quit()
-        
-    def start_window(self):    
-        self.start_boxes()
-        self.start_labels()
-        self.start_buttons()
-        self.add(self.wM.boxes[2])                                      #Afegim la capsa principal a la finestra, aquesta capsa conté tots els wadgets    
-        self.show_all()
         
     def start_boxes(self)
           """Per tal de distribuir les capses de forma que quedi una a sobre de la altre, creo una capsa vertical i considero la capsa inferior i superior com si fossing wadgets. 
         Si configuro que la caixa superior tingui els paràmetrs expand i fill com a True i la inferior com a False, en l'ordre que s'executa el codi quedaràn col·locades com s'espera"""
         
-        self.wM.create_box(Gtk.Orientation.HORIZONTAL)                                  #Creem la capsa 0 (inferior)
-        self.wM.create_box(Gtk.Orientation.HORIZONTAL)                                  #Creem la capsa 1 (superior)
-        self.wM.create_box(Gtk.Orientation.VERTICAL)                                    #Creem la capsa 2 (conté les capses 0 i 1)
+        self.wM.create_box(Gtk.Orientation.HORIZONTAL,0)                                  #Creem la capsa 0 (inferior)
+        self.wM.create_box(Gtk.Orientation.HORIZONTAL,0)                                  #Creem la capsa 1 (superior)
+        self.wM.create_box(Gtk.Orientation.VERTICAL,0)                                    #Creem la capsa 2 (conté les capses 0 i 1)
 
         self.wM.add_widget_box(self.wM.boxes[2],self.wM.boxs[1],True,True,0)             #Afegim la capsa 1 (superior) a la capsa 2 (principal)
-        self.wM.add_widget_box(self.wM.boxes[2],self.wM.boxs[0],False,False,0)           #Afegim la capsa 2 (superior) a la capsa 2 (principal)
+        self.wM.add_widget_box(self.wM.boxes[2],self.wM.boxs[0],False,False,0)           #Afegim la capsa 0 (inferior) a la capsa 2 (principal)
 
     def start_labels(self)
         self.wM.create_label(""""                    Benvingut!
                                           Siusplau, identifique-vos apropant el vostre carnet de la UPC """)
-        self.wE.configure_style(self.wM.labels[0],"#4682B4","black","0","0")              #Configurem l'estil del Label de benvinguda
+        self.wE.configure_style(self.wM.labels[0],"#4682B4","black","0","0")               #Configurem l'estil del Label de benvinguda
         self.wM.add_widget_box(self.wM.boxes[1],self.wM.labels[0],True,True,0)             #Afegim el label de benvinguda a la capsa 1 (superior)
         
     def start_buttons(self)
         self.wM.create_button("Surt")                                                      #Creem el botó de sortida
         self.wM.buttons[0].connect("clicked",self.exit_buttom_pressed)                     #S'executarà la funció "exit_buttom_pressed quan pressionem el botó
         self.wM.configure_style(self.wM.buttons[0],"red","black","0","20")                 #Configurem l'estil del botó de sortida
-        self.wM.buttons[0].set_halign(Gtk.Align.START)                                     # Situem el botó de sortida a la esquerra de la caixa
+        self.wM.buttons[0].set_halign(Gtk.Align.START)                                     #Situem el botó de sortida a la esquerra de la caixa
         self.wM.add_widget_box(self.wM.boxs[0],self.wM.buttons[0],False,False,0)           #Afegim el botó "Surt" a la capsa 0 (inferior)
+        
+     def start_window(self):    
+        self.start_boxes()
+        self.start_labels()
+        self.start_buttons()
+        self.add(self.wM.boxes[2])                           #Afegim la capsa principal a la finestra, aquesta capsa conté tots els wadgets    
+        self.show_all()
+         
+    def exit_button_pressed(self):
+        Gtk.main_quit()
         
 class widgetManager:
     def __init__(self):
@@ -62,8 +62,8 @@ class widgetManager:
     def create_label(self,text):
         self.labels.append(Gtk.Label(label=text))
         
-    def create_box(self,orientació):
-        box = Gtk.Box(orientation=orientació,spacing=1)
+    def create_box(self,orientation,spacing):
+        box = Gtk.Box(orientation=orientation,spacing=spacing)
         self.boxes.append(box)
                
     def create_button(self,text):
@@ -87,8 +87,9 @@ class widgetManager:
                                                     
                     
 if __name__ == "__main__":
-      finestra = Finestra(widgetManager())
-      finestra.configure_window()
+      window = MyWindow(widgetManager())
+      window.configure_window(400,300)
+      window.start_window()
       Gtk.main()
     
 
