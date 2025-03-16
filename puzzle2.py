e1e430952441b2847f37c4d3dd27fdafeea6f164
hugoroldan24 <hugoroldan060@gmail.com>
2025-03-16 15:53:44 +0100

    Update puzzle2.py

--- puzzle2.py
+++ puzzle2.py
@@ -1,20 +1,19 @@
-"""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 En el puzzle2 es demana crear una versió gràfica del puzzle1. S'utilitzarà la la biblioteca PyGObject. PyGObject es un binding que permet utilitzar les llibreries basades en GObject com GTK3 que estàn escrites amb C però desde Python.
 Per instalar aquesta biblioteca fem: sudo pip3 install PyGObject. Seguidament, descarreguem el paquet gtk3 que es el que conté les funcions per crear la interfaç. sudo apt install python3-gi (bindings de GTK para python, sudo apt install libgtk-3-dev. Finalment, instalem el paquet gir1.2-gtk-3.0. Aquest paquet permet a la llibreria PyGObject accedir a les funcionalitats de la llibreria gtk3 desde Python.
 sudo apt install gir1.2-gtk-3.0
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-"""
 import gi
 import puzzle1 
 import threading 
 gi.require_version("Gtk", "3.0")                              #Indiquem que volem fer servir GTK3
 from gi.repository import Gtk, GLib                           #Importem desde el repositori de gi la llibreria Gtk i GLib que conté les classes i mètodes per crear la interfaç i interactuar amb threads auxiliars respectivament. 
 
-WELCOME_STRING = """                    Benvingut!                            
-                                          Siusplau, identifique-vos apropant el vostre carnet de la UPC """
+WELCOME_STRING = "Please, login with your university card"
+GREEN_COLOR = Gdk.RGBA(0.0, 1.0, 0.0, 1.0)                    #Color verd en format RGBA (R=0, G=1, B=0, A=1)
+BLUE_COLOR = Gdk.RGBA(0.0, 0, 1, 1.0)                         #Color blau en format RGBA (R=0, G=0, B=1, A=1)
 
-GREEN_COLOR = Gdk.RGBA(0.0, 1.0, 0.0, 1.0)  # Color verd en format RGBA (R=0, G=1, B=0, A=1)
-BLUE_COLOR = Gdk.RGBA(0.0, 0, 1, 1.0)  # Color blau en format RGBA (R=0, G=0, B=1, A=1)
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 Classe per configurar la finestra d'una aplicació i els seus elements. La classe hereda la classe Gtk.ApplicationWindow
@@ -26,7 +25,7 @@ class MyWindow(Gtk.ApplicationWindow):
         :widgetManager: Objecte de la classe widgetManager per gestionar els widgets
     """
     def __init__(self,widgetManager):
-        super().__init__()                                                #Truquem a la funció __init__ de la classe Gtk.ApplicationWindow.
+        super().__init__()                                                 #Cridem a la funció __init__ de la classe Gtk.ApplicationWindow.
         self.wM = widgetManager                                 
         self.myReader = puzzle1.Rfid_522()                                 #Instancia un objecte de la classe Rfid_522() de la llibreria puzzle1.
       
@@ -57,6 +56,7 @@ class MyWindow(Gtk.ApplicationWindow):
         self.welcome_label = self.wM.create_label(WELCOME_STRING)                                            #Creem el label amb el text passat per paràmetre
         self.wM.add_widget_box_start(self.main_box,self.welcome_label, True, True, 0)                        #Afegim el label al inici de la capsa vertical (part superior)
         self.wM.set_widget_name(self.welcome_label,"welcome_label")                                          #Posem un nom al widget per tal de aplicar-li les regles CSS
+      
     """
     Instancia els botons que es faràn servil inicialmente. Configura les seves característiques i els afegeix a la Capsa 0.
     """
@@ -109,10 +109,9 @@ class MyWindow(Gtk.ApplicationWindow):
         :uid: Identificador de la tarjeta obtingut a la lectura.
     """
     def update_window(self,uid):
-        self.wM.change_background_color(self.welcome_label,GREEN_COLOR)                                   #Posem el fons del label de color verd.
-        self.welcome_label.set_text(f"""                    Tarjeta detectada satisfactòriament!
-                                                                     uid: {uid}""")
-        self.wM.set_widget_visible(self.clear_button,True)                                                #Fem que el botó "Clear" es torni visible.
+        self.wM.change_background_color(self.welcome_label,GREEN_COLOR)                                      #Posem el fons del label de color verd.
+        self.welcome_label.set_text(f"uid: {uid}")                                                                                     
+        self.wM.set_widget_visible(self.clear_button,True)                                                   #Fem que el botó "Clear" es torni visible.
          
     """
     Torna la finestra a l'estat inicial un cop polsem el botó "Clear".
@@ -123,14 +122,15 @@ class MyWindow(Gtk.ApplicationWindow):
          self.set_widget_visible(self.clear_button,False)                                                    #Tornem a fer el botó "Clear" invisible
          self.myReader.uid = None                                                                            #Esborrem la uid prèvia
          self.start_reading_thread()                                                                         #Tornem a executar el fil secundari per poder tornar a lleguir una uid.
+      
         
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 Aquesta classe s'encarrega de fer una total gestió dels widgets. La seva funció es crear i modificar els widgets de la finestra.
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 class widgetManager:
     def __init__(self):         
-        self.editor_css = Gtk.CssProvider()                                                              #Creem l'objecte que controlarà les regles d'estil CSS.   
-          
+        self.editor_css = Gtk.CssProvider()                                                                  #Creem l'objecte que controlarà les regles d'estil CSS.   
+      
    """
    Crea un label.
    Paràmetres:
@@ -139,9 +139,8 @@ class widgetManager:
        Label: Torna un label amb el text especificat
    """   
    def create_label(self,text):             
-      return Gtk.Label(label=text)                                                                       #Creem un label i el retornem.
-          
-        
+      return Gtk.Label(label=text)                                                                           #Creem un label i el retornem.
+                  
     """
     Crea una capsa.
     Paràmetres:
@@ -151,7 +150,7 @@ class widgetManager:
         Box: Torna una capsa amb els paràmetres especificats
     """
     def create_box(self,orientation,spacing): 
-        return Gtk.Box(orientation=orientation,spacing=spacing)                                         #Creem la capsa i la retornem.
+        return Gtk.Box(orientation=orientation,spacing=spacing)                                             #Creem la capsa i la retornem.
         
     """
     Crea un botó. 
@@ -161,7 +160,7 @@ class widgetManager:
         Button: Tornem un botó amb el text especificat
     """          
     def create_button(self,text):         
-       return Gtk.Button(label=text)                                                                     #Creem el botó i el retornem         
+       return Gtk.Button(label=text)                                                                         #Creem el botó i el retornem         
             
     """
     Afegim el widget al inici de la capsa passada per argument.
@@ -187,41 +186,47 @@ class widgetManager:
     def add_widget_box_end(self,box,widget,expand,fill,padding):
         box.pack_end(widget, expand, fill, padding)
       
+    """
+    Configurem el nom del widget
+    """
     def set_widget_name(self,widget,name)
         widget.set_name(name)
       
+    """
+    Fem el widget visible (visibility = True) o invisible (visibility = False)
+    """
     def set_widget_visible(self,widget,visibility)
         widget.set_visible(visibility)
    
-
     """
     Funció que modifica el color de fons de un widget. És útil si volem modificar el color de forma dinàmica un cop ja hem aplicat les regles CSS
     Paràmetres:
         :widget: Widget que volem editar.
         :color: Color que volem introduir. Ha de ser un objecte de la glase Gtk.RGBA.
     """
-        def change_backgroud_color(self,widget,color)
-            widget.override_background_color(Gtk.StateFlags.NORMAL,color)
+    def change_backgroud_color(self,widget,color)
+        widget.override_background_color(Gtk.StateFlags.NORMAL,color)
+      
     """
-    Utilitzem el llenguatge CSS per editar els widgets.
+    Funció que aplica les regles CSS als widgets.
     Paràmetres:
-        :widget: Widget que volem editar.
+      
         :color_fons: Color desijat del fons del widget.
         :color_text: Color desijat del text del widget.
         :padding: Marge entre el text i la seva vora.
         :border_radius: Radi de curvatura de la vora del widget.
     """
-    def configure_style_CSS(self):                                                                                                #Creem la cadena de text que conté regles CSS dinàmicament utilitzant f-strings.
-      css = b"""                                                                         
+    def configure_style_CSS(self):                                   
+      css = b"""                                    #Creem la cadena de text que conté regles CSS dinàmicament utilitzant f-strings.                                                                         
         #welcome_label{
-            background-color: blue;  
-            color: black;                  
-            padding: 60px;                  
-            border-radius: 10px;
-            margin-left: 5px;
-            margin-right: 5px;
-            margin-bottom: 
-            font-size: 
+            background-color: blue;                 #Color desijat del fons del widget.                                                                                                                             
+            color: black;                           #Color desijat del text del widget.                  
+            padding: 60px;                          #Marge entre el text i la seva vora.            
+            border-radius: 10px;                    #Radi de curvatura de la vora del widget.
+            margin-left: 5px;                       #Marge esquerra
+            margin-right: 5px;                      #Marge dret
+            margin-bottom:                          #Marge inferior 
+            font-size:                              #Tamany del text
             }
         #exit_button{
             background-color: red;  
@@ -239,13 +244,13 @@ class widgetManager:
             }
                             
          """
-        self.editor_css.load_from_data(css)                                             #Carreguem les regles d'estil CSS del string "css" en format de bytes al proveïdor CSS que hem instanciat al mètode __init__.
-        self.get_style_context().add_provider(self.editor_css,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)  
+        self.editor_css.load_from_data(css)                                                             #Carreguem les regles d'estil CSS del string "css" al proveïdor CSS que hem instanciat al mètode __init__.
+        self.get_style_context().add_provider(self.editor_css,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)  #Apliquem les regles CSS als widgets.
 
 
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-Classe que permet gestionar la aplicació principal, gestiona les finestres. En aquest cas només hem de gestiona una finestra.
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Classe que permet gestionar la aplicació principal, gestiona les finestres.
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 class Application(Gtk.Application):
     def __init__(self):
         super().__init__()         
@@ -255,14 +260,15 @@ class Application(Gtk.Application):
     def do_activate(self):       
         self.window = MyWindow(widgetManager())                                          #Instanciem una finestra i passem un objecte widgetManager per paràmetre.
         self.window.configure_window(400,300,Gtk.WindowPosition.CENTER,"PUZZLE2")        #Configurem la finestra.
-        self.window.connect("destroy",self.quit)                                         #La finestra es podrà esborrar de forma manual eliminant la pestanya o clicant a la X.
+        self.window.connect("destroy", Gtk.main_quit)                                    #La finestra es podrà esborrar de forma manual eliminant la pestanya o clicant a la X.
         self.window.start_window()                                                       #Arranquem la finestra.
         self.window.present()                                                            #Mostrem la finestra.
-        Gtk.main()
-                                                                        
+        Gtk.main()                                                                       #Permet mantenir la finestra oberta i respondre a event com clicar un botó.
+
+
 if __name__ == "__main__":        
-      app = Application()                                                                #Instanciem un objecte de la classe Application
-      app.run()                                                                          #Arrenquem la aplicació
+       app = Application()                                                                #Instanciem un objecte de la classe Application
+       app.run()                                                                          #Arrenquem la aplicació
     
 
 
